da52f77ec38d4ae1dfacdb9654343a6aeffb0b7e
Guang Ya Liu <liugya@cn.ibm.com>
2016-06-03 15:25:27 +0800

    Update dashboard

--- src/js/pages/DashboardPage.js
+++ src/js/pages/DashboardPage.js
@@ -267,6 +267,39 @@ var DashboardPage = React.createClass({
           </div>
           <div className="grid-item column-mini-6 column-large-4 column-x-large-3">
             <Panel
+              className="panel panel-inverse dashboard-panel dashboard-panel-list dashboard-panel-list-service-health allow-overflow"
+              heading={this.getHeading('Kubernetes Services Health')}
+              headingClass="panel-header panel-header-bottom-border inverse short-top short-bottom">
+              <ServiceList
+                healthProcessed={appsProcessed}
+                services={this.getServicesList(data.services.getItems())} />
+              {this.getViewAllServicesBtn()}
+            </Panel>
+          </div>
+          <div className="grid-item column-mini-6 column-large-4 column-x-large-3">
+            <Panel
+              className="panel panel-inverse dashboard-panel dashboard-panel-list dashboard-panel-list-service-health allow-overflow"
+              heading={this.getHeading('Kubernetes Pod Health')}
+              headingClass="panel-header panel-header-bottom-border inverse short-top short-bottom">
+              <ServiceList
+                healthProcessed={appsProcessed}
+                services={this.getServicesList(data.services.getItems())} />
+              {this.getViewAllServicesBtn()}
+            </Panel>
+          </div>
+          <div className="grid-item column-mini-6 column-large-4 column-x-large-3">
+            <Panel
+              className="panel panel-inverse dashboard-panel dashboard-panel-list dashboard-panel-list-service-health allow-overflow"
+              heading={this.getHeading('Kubernetes Replication Controller Health')}
+              headingClass="panel-header panel-header-bottom-border inverse short-top short-bottom">
+              <ServiceList
+                healthProcessed={appsProcessed}
+                services={this.getServicesList(data.services.getItems())} />
+              {this.getViewAllServicesBtn()}
+            </Panel>
+          </div>
+          <div className="grid-item column-mini-6 column-large-4 column-x-large-3">
+            <Panel
               className="panel panel-inverse dashboard-panel"
               heading={this.getHeading('Tasks')}
               headingClass="panel-header panel-header-bottom-border inverse short-top short-bottom">
